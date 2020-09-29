5af78269607ce55ae4a1423bd4dc0da5a6501424
Thomas, Biju (bt2983) <bt2983@us.att.com>
2020-09-29 15:51:01 -0600

    more changes

---
+++ add-router.sh
@@ -0,0 +1,8 @@
+name="testrouter"$(date "+%Y-%m-%d--%H-%M-%S")
+
+echo $name
+
+echo '{ "source": "ServiceNow", "attributes": { "name":"' $name '" } } ' \
+	| curl -H "Content-Type: application/json" -X POST --data-binary @- \
+		--user $SN_USER:$SN_PASS $SN_PROXY \
+		$SN_URL/api/now/cmdb/instance/cmdb_ci_ip_router 
--- get-test.sh
+++ get-test.sh
@@ -1,1 +1,2 @@
-curl --user $SN_USER:$SN_PASS $SN_PROXY $SN_URL/api/now/table/incident?sysparm_limit=1
+curl --user $SN_USER:$SN_PASS $SN_PROXY \
+	$SN_URL/api/now/table/incident?sysparm_limit=1
